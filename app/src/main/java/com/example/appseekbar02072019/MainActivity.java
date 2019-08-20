e4dbcf3651899462822837cb8b9ce4fe6760fd4a
phatdroid94 <phatdroid94@gmail.com>
2019-08-20 18:08:13 +0700

    A

--- app/src/main/java/com/example/appseekbar02072019/MainActivity.java
+++ app/src/main/java/com/example/appseekbar02072019/MainActivity.java
@@ -8,16 +8,39 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekbar);
+
+        seekBar.setMax(10000);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.d("BBB",i + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
